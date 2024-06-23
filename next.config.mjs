7ca4720ac8028451b3711bdc723ce0c425512db5
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2024-06-23 18:22:52 +0800

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    API_KEY: process.env.API_KEY,
+  },
 };
 
 export default nextConfig;
