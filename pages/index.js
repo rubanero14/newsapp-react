16f58bd91920c8a55a11706ae6a1af6c665a3832
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2024-06-27 23:11:03 +0800

    ui fix

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
     setIsLoading(true);
     await axios
       .get(
-        `http://localhost:4000/newsapi/data/${feedtype}/${
+        `https://cms-backend-1yrg.vercel.app/newsapi/data/${feedtype}/${
           query === "" ? undefined : query
         }/${category}/${country}`
       )
